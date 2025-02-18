50caa0006f1679437ab6a8706d1ed6c5e2c72f4a
ritesh-raushan <rausahnritesh@gmail.com>
2025-02-18 15:33:41 +0530

    Add About and Contact Pages

---
+++ src/app/about/page.js
@@ -0,0 +1,9 @@
+
+export default function AboutPage() {
+    return (
+        <div className="flex items-center justify-center min-h-screen">
+            <span className="text-[#00ffee] text-4xl">About</span>
+            <span className="text-white text-4xl ml-2">Page</span>
+        </div>
+    );
+}
---
+++ src/app/contact/page.js
@@ -0,0 +1,9 @@
+
+export default function AboutPage() {
+    return (
+        <div className="flex items-center justify-center min-h-screen">
+            <span className="text-[#00ffee] text-4xl">Contact</span>
+            <span className="text-white text-4xl ml-2">Page</span>
+        </div>
+    );
+}
